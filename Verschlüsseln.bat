ce9fb9da4e92e58b4fe37e080202638ff4745129
dos7ba <Stefan.Dorn2@de.bosch.com>
2024-04-19 06:29:13 +0200

    Passworteingabe notwendig

--- Verschlüsseln.bat
+++ Verschlüsseln.bat
@@ -1,8 +1,10 @@
 echo off
 set content=%~1\*
-set password=TEST
 set outfile=%~1_Datum_Uhrzeit
+
+set /p password=Bitte Passwort eingeben: [32m
 echo on
+
 "%~dp0/7zip/7z.exe" a -saa -tzip -stl -p%password% -mem=AES256 "%outfile%"  "%content%"
 rem Pfad zum Tool testen
 rem Abfrage Password
